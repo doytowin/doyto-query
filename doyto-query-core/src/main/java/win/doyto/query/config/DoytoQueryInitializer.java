9ae8f2122e6fe14dc0278b47e927a07226b532cd
huguanglei <guanglei.hu@baozun.com>
2019-05-30 13:33:57 +0800

    [doyto-query] DoytoQueryInitializer

---
+++ doyto-query-core/src/main/java/win/doyto/query/config/DoytoQueryInitializer.java
@@ -0,0 +1,23 @@
+package win.doyto.query.config;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import win.doyto.query.core.GlobalConfiguration;
+
+/**
+ * DoytoQueryInitializer
+ *
+ * @author f0rb
+ * @date 2019-05-20
+ */
+public class DoytoQueryInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    private static final String MAP_CAMEL_CASE_TO_UNDERSCORE = "doyto.query.config.map-camel-case-to-underscore";
+
+    @Override
+    public void initialize(ConfigurableApplicationContext context) {
+        ConfigurableEnvironment environment = context.getEnvironment();
+        String enabled = environment.getProperty(MAP_CAMEL_CASE_TO_UNDERSCORE, "false");
+        GlobalConfiguration.instance().setMapCamelCaseToUnderscore(Boolean.valueOf(enabled));
+    }
+}
