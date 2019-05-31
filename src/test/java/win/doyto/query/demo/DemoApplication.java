897d9afcf29f675374730a05e716c15260376c3f
袁臻 <f0rb@163.com>
2019-05-31 23:30:58 +0800

    [doyto-query]jpa to jdbc

--- src/test/java/win/doyto/query/demo/DemoApplication.java
+++ src/test/java/win/doyto/query/demo/DemoApplication.java
@@ -3,6 +3,11 @@ package win.doyto.query.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 /**
  * DemoApplication
@@ -14,5 +19,10 @@ import org.springframework.cache.annotation.EnableCaching;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class);
+    }
+
+    @Resource
+    public void setDataSource(DataSource dataSource) {
+        new ResourceDatabasePopulator(new ClassPathResource("import.sql")).execute(dataSource);
     }
 }
