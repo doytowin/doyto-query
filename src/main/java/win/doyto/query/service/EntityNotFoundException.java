48cdb26a511ac353cbd98a7e1ae4310222d9bda2
Forb Yuan <f0rb@163.com>
2019-07-11 15:39:29 +0800

    init EntityNotFoundException with message

--- src/main/java/win/doyto/query/service/EntityNotFoundException.java
+++ src/main/java/win/doyto/query/service/EntityNotFoundException.java
@@ -6,4 +6,7 @@ package win.doyto.query.service;
  * @author f0rb on 2019-06-10
  */
 public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException() {
+        super("Entity not found!");
+    }
 }
