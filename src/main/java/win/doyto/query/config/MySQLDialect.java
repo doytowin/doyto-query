19bdb17a15a100cc37676b21360393c9052a2199
Forb Yuan <f0rb@163.com>
2020-04-18 12:30:39 +0800

    MySQLDialect.java

--- src/main/java/win/doyto/query/config/MySQLDialect.java
+++ src/main/java/win/doyto/query/config/MySQLDialect.java
@@ -2,14 +2,44 @@ package win.doyto.query.config;
 
 import win.doyto.query.core.Dialect;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * MySQLDialect
  *
  * @author f0rb on 2019-07-22
  */
-class MySQLDialect implements Dialect {
+public class MySQLDialect implements Dialect {
+    private static final String LIMIT = " LIMIT ";
+    private Pattern from = Pattern.compile("FROM \\w+");
+    private Pattern join = Pattern.compile("join", Pattern.CASE_INSENSITIVE);
+    private Pattern alias = Pattern.compile("(,|SELECT)\\s*(\\w+)");
+
     @Override
     public String buildPageSql(String sql, int limit, long offset) {
-        return sql + " LIMIT " + limit + (sql.startsWith("SELECT") ? " OFFSET " + offset : "");
+        if (!sql.startsWith("SELECT")) {
+            return sql + LIMIT + limit;
+        }
+        Matcher fromMatcher = from.matcher(sql);
+        if (!fromMatcher.find() || join.matcher(sql).find()) {
+            return sql + LIMIT + limit + (sql.startsWith("SELECT") ? " OFFSET " + offset : "");
+        }
+        String fromTable = fromMatcher.group();
+        int fromIndex = fromMatcher.start();
+
+        StringBuffer start = new StringBuffer(sql.length() * 3);
+        Matcher aliasMatcher = alias.matcher(sql.substring(0, fromIndex));
+        while (aliasMatcher.find()) {
+            aliasMatcher.appendReplacement(start, "$1 a.$2");
+        }
+        aliasMatcher.appendTail(start);
+        start.append(fromTable)
+             .append(" a JOIN (SELECT id ")
+             .append(sql.substring(fromIndex))
+             .append(LIMIT).append(limit).append(" OFFSET ").append(offset)
+             .append(") b ON a.id = b.id");
+        return start.toString();
+
     }
 }
---
+++ src/test/java/win/doyto/query/config/MySQLDialectTest.java
@@ -0,0 +1,40 @@
+package win.doyto.query.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * MySQLDialectTest
+ *
+ * @author f0rb on 2020-04-03
+ */
+class MySQLDialectTest {
+    @Test
+    void page() {
+        MySQLDialect mysqlDialect = new MySQLDialect();
+        String pageSql = mysqlDialect.buildPageSql("SELECT username, password FROM user WHERE valid = true", 10, 50000);
+        assertEquals("SELECT a.username, a.password FROM user a JOIN (SELECT id FROM user WHERE valid = true LIMIT 10 OFFSET 50000) b ON a.id = b.id", pageSql);
+    }
+
+    @Test
+    void update() {
+        MySQLDialect mysqlDialect = new MySQLDialect();
+        String pageSql = mysqlDialect.buildPageSql("UPDATE user SET user_level = ?, valid = ? WHERE id = ?", 10, 20);
+        assertEquals("UPDATE user SET user_level = ?, valid = ? WHERE id = ? LIMIT 10", pageSql);
+    }
+
+    @Test
+    void delete() {
+        MySQLDialect mysqlDialect = new MySQLDialect();
+        String pageSql = mysqlDialect.buildPageSql("DELETE FROM user WHERE username = ?", 10, 20);
+        assertEquals("DELETE FROM user WHERE username = ? LIMIT 10", pageSql);
+    }
+
+    @Test
+    void join() {
+        MySQLDialect mysqlDialect = new MySQLDialect();
+        String pageSql = mysqlDialect.buildPageSql("SELECT u.username, ur.role_id FROM t_user u LEFT JOIN t_user_and_role ur ON u.id = ur.user_id", 10, 20);
+        assertEquals("SELECT u.username, ur.role_id FROM t_user u LEFT JOIN t_user_and_role ur ON u.id = ur.user_id LIMIT 10 OFFSET 20", pageSql);
+    }
+}
