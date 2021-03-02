cf8ea838a7e7b48f64abc0b4e4e3952dc68c5b13
Forb Yuan <f0rb@163.com>
2021-03-02 23:25:43 +0800

    :recycle: MySQLDialect

--- src/main/java/win/doyto/query/config/MySQLDialect.java
+++ src/main/java/win/doyto/query/config/MySQLDialect.java
@@ -12,8 +12,9 @@ import java.util.regex.Pattern;
  */
 public class MySQLDialect implements Dialect {
     private static final String LIMIT = " LIMIT ";
-    private Pattern from = Pattern.compile("FROM \\w+");
-    private Pattern join = Pattern.compile("join", Pattern.CASE_INSENSITIVE);
+    private static final String OFFSET = " OFFSET ";
+    private Pattern fromPtn = Pattern.compile("FROM \\w+", Pattern.CASE_INSENSITIVE);
+    private Pattern joinPtn = Pattern.compile("JOIN", Pattern.CASE_INSENSITIVE);
     private Pattern alias = Pattern.compile("(,|SELECT)\\s*([\\w*]+)");
 
     @Override
@@ -21,25 +22,30 @@ public class MySQLDialect implements Dialect {
         if (!sql.startsWith("SELECT")) {
             return sql + LIMIT + limit;
         }
-        Matcher fromMatcher = from.matcher(sql);
-        if (!fromMatcher.find() || join.matcher(sql).find()) {
-            return sql + LIMIT + limit + (sql.startsWith("SELECT") ? " OFFSET " + offset : "");
+        if (joinPtn.matcher(sql).find()) {
+            return sql + LIMIT + limit + OFFSET + offset;
         }
+        return buildPageForSelect(sql, limit, offset);
+    }
+
+    private String buildPageForSelect(String sql, int limit, long offset) {
+        Matcher fromMatcher = fromPtn.matcher(sql);
+        fromMatcher.find();
         String fromTable = fromMatcher.group();
         int fromIndex = fromMatcher.start();
 
-        StringBuffer start = new StringBuffer(sql.length() * 3);
+        StringBuffer buffer = new StringBuffer(sql.length() * 3);
         Matcher aliasMatcher = alias.matcher(sql.substring(0, fromIndex));
         while (aliasMatcher.find()) {
-            aliasMatcher.appendReplacement(start, "$1 a.$2");
+            aliasMatcher.appendReplacement(buffer, "$1 a.$2");
         }
-        aliasMatcher.appendTail(start);
-        start.append(fromTable)
-             .append(" a JOIN (SELECT id ")
-             .append(sql.substring(fromIndex))
-             .append(LIMIT).append(limit).append(" OFFSET ").append(offset)
-             .append(") b ON a.id = b.id");
-        return start.toString();
-
+        aliasMatcher.appendTail(buffer);
+        buffer.append(fromTable)
+              .append(" a JOIN (SELECT id ")
+              .append(sql.substring(fromIndex))
+              .append(LIMIT).append(limit)
+              .append(OFFSET).append(offset)
+              .append(") b ON a.id = b.id");
+        return buffer.toString();
     }
 }
