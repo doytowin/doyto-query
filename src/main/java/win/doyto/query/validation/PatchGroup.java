e5e220431da49a9c4f2af5ee6eafeb58be8d9e28
Forb Yuan <f0rb@163.com>
2021-03-07 23:58:32 +0800

    migrate validation group here

---
+++ src/main/java/win/doyto/query/validation/PatchGroup.java
@@ -0,0 +1,11 @@
+package win.doyto.query.validation;
+
+/**
+ * PatchGroup
+ *
+ * @deprecated move into package win.doyto.query.web.validation
+ */
+@SuppressWarnings({"java:S1133","java:S2176"})
+@Deprecated
+public interface PatchGroup extends win.doyto.query.web.validation.PatchGroup {
+}
