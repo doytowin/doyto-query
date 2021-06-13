b612f92543b61c532246272e14b2edce033ffe34
Forb Yuan <f0rb@163.com>
2021-06-13 17:57:59 +0800

    deprecate CreateGroup.java/PatchGroup.java/UpdateGroup.java

--- src/main/java/win/doyto/query/validation/CreateGroup.java
+++ src/main/java/win/doyto/query/validation/CreateGroup.java
@@ -6,6 +6,9 @@ import javax.validation.groups.Default;
  * CreateGroup
  *
  * @author f0rb on 2019-05-28
+ * @deprecated move into package win.doyto.query.web.validation
  */
+@SuppressWarnings({"java:S1133", "java:S2176"})
+@Deprecated
 public interface CreateGroup extends Default {
 }
--- src/main/java/win/doyto/query/validation/PatchGroup.java
+++ src/main/java/win/doyto/query/validation/PatchGroup.java
@@ -5,7 +5,9 @@ import javax.validation.groups.Default;
 /**
  * PatchGroup
  *
- * @author f0rb on 2019-05-28
+ * @deprecated move into package win.doyto.query.web.validation
  */
+@SuppressWarnings({"java:S1133", "java:S2176"})
+@Deprecated
 public interface PatchGroup extends Default {
 }
