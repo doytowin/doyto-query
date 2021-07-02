4abc21bfa56a885087f412a3ee31e6eaf4f37cab
Forb Yuan <f0rb@163.com>
2021-07-02 23:08:02 +0800

    :recycle: rename import.sql to schema.sql

--- src/test/java/win/doyto/query/demo/DemoApplication.java
+++ src/test/java/win/doyto/query/demo/DemoApplication.java
@@ -4,16 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import win.doyto.query.service.AssociativeService;
 import win.doyto.query.service.TemplateAssociativeService;
 import win.doyto.query.web.WebMvcConfigurerAdapter;
 import win.doyto.query.web.component.ListValidator;
-
-import javax.annotation.Resource;
-import javax.sql.DataSource;
 
 /**
  * DemoApplication
@@ -26,11 +21,6 @@ import javax.sql.DataSource;
 public class DemoApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class);
-    }
-
-    @Resource
-    public void setDataSource(DataSource dataSource) {
-        new ResourceDatabasePopulator(new ClassPathResource("import.sql")).execute(dataSource);
     }
 
     @Bean
