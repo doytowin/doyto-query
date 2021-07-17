dfbde0ffb3d6b8915c46fc023aadf16956e3120e
Forb Yuan <f0rb@163.com>
2021-07-17 22:59:08 +0800

    update spring boot to 2.2.13.RELEASE

--- src/test/java/win/doyto/query/demo/test/DemoApplicationTest.java
+++ src/test/java/win/doyto/query/demo/test/DemoApplicationTest.java
@@ -2,6 +2,7 @@ package win.doyto.query.demo.test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
@@ -10,7 +11,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import win.doyto.query.demo.DemoApplication;
@@ -31,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @SpringBootTest(classes = DemoApplication.class)
+@AutoConfigureMockMvc
 public abstract class DemoApplicationTest {
 
     @Resource
@@ -62,8 +63,6 @@ public abstract class DemoApplicationTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-
         MvcResult mvcResult = requestJson(post("/login"), "{\"account\":\"f0rb\",\"password\":\"123456\"}")
                 .andExpect(statusIs200()).andReturn();
         session = (MockHttpSession) mvcResult.getRequest().getSession();
