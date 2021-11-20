a698247cf2352afc9a67e2572618c38577c7310c
Forb Yuan <f0rb@163.com>
2021-11-20 10:10:21 +0800

    [doyto-query-webflux] R2dbcTemplateTest#count

--- doyto-query-webflux/src/main/java/win/doyto/query/data/R2dbcTemplate.java
+++ doyto-query-webflux/src/main/java/win/doyto/query/data/R2dbcTemplate.java
@@ -1,5 +1,8 @@
 package win.doyto.query.data;
 
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.Statement;
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import win.doyto.query.core.SqlAndArgs;
@@ -9,7 +12,10 @@ import win.doyto.query.core.SqlAndArgs;
  *
  * @author f0rb on 2021-11-20
  */
+@RequiredArgsConstructor
 public class R2dbcTemplate implements R2dbcOperations {
+
+    private final ConnectionFactory connectionFactory;
 
     @Override
     public <V> Flux<V> query(SqlAndArgs sqlAndArgs, RowMapper<V> rowMapper) {
@@ -18,7 +24,14 @@ public class R2dbcTemplate implements R2dbcOperations {
 
     @Override
     public Mono<Long> count(SqlAndArgs sqlAndArgs) {
-        return Mono.just(3L);
+        return Mono.from(connectionFactory.create())
+                   .flatMapMany(
+                           connection -> {
+                               Statement statement = connection.createStatement(sqlAndArgs.getSql());
+                               return statement.execute();
+                           })
+                   .flatMap(result -> result.map((row, rowMetadata) -> row.get(0, Long.class)))
+                   .singleOrEmpty();
     }
 
     @Override
