51be5b22733086ac6d3594583853374ebdf5ad09
Forb Yuan <f0rb@163.com>
2021-11-27 23:12:21 +0800

    [doyto-query-web] fix tests

--- doyto-query-web/src/test/java/win/doyto/query/web/demo/DemoApplication.java
+++ doyto-query-web/src/test/java/win/doyto/query/web/demo/DemoApplication.java
@@ -2,8 +2,13 @@ package win.doyto.query.web.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import win.doyto.query.data.DatabaseOperations;
+import win.doyto.query.data.DatabaseTemplate;
 import win.doyto.query.web.WebMvcConfigurerAdapter;
 
 /**
@@ -17,5 +22,11 @@ import win.doyto.query.web.WebMvcConfigurerAdapter;
 public class DemoApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class);
+    }
+
+    @Bean
+    @ConditionalOnClass({JdbcOperations.class})
+    public DatabaseOperations dataOperations(JdbcOperations jdbcOperations) {
+        return new DatabaseTemplate(jdbcOperations);
     }
 }
