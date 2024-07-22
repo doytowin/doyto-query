f07c1e69c0c592cf9e9f8ffb4ce5ae9780010873
Forb Yuan <f0rb@163.com>
2024-07-22 16:56:31 +0800

    #156 add DataAccess#page(Query)

--- doyto-query-web/src/main/java/win/doyto/query/service/CachedDataAccess.java
+++ doyto-query-web/src/main/java/win/doyto/query/service/CachedDataAccess.java
@@ -17,6 +17,7 @@
 package win.doyto.query.service;
 
 import lombok.Getter;
+import lombok.experimental.Delegate;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.cache.CacheManager;
@@ -39,6 +40,7 @@ import java.util.List;
 public class CachedDataAccess<E extends Persistable<I>, I extends Serializable, Q extends DoytoQuery>
         implements DataAccess<E, I, Q> {
 
+    @Delegate
     @Getter
     private final DataAccess<E, I, Q> delegate;
 
@@ -83,11 +85,6 @@ public class CachedDataAccess<E extends Persistable<I>, I extends Serializable, 
         }
     }
 
-    @Override
-    public List<I> queryIds(Q query) {
-        return delegate.queryIds(query);
-    }
-
     protected String generateCacheKey(Q query) {
         if (TransactionSynchronizationManager.isActualTransactionActive()) {
             return null;
@@ -99,16 +96,6 @@ public class CachedDataAccess<E extends Persistable<I>, I extends Serializable, 
     public List<E> query(Q query) {
         String key = generateCacheKey(query);
         return queryCacheWrapper.execute(key, () -> delegate.query(query));
-    }
-
-    @Override
-    public long count(Q query) {
-        return delegate.count(query);
-    }
-
-    @Override
-    public <V> List<V> queryColumns(Q q, Class<V> clazz, String... columns) {
-        return delegate.queryColumns(q, clazz, columns);
     }
 
     @Override
